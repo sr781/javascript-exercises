0bf1d069f78f602a93e2c701952026a3730ec4cf
sr781 <96390217+sr781@users.noreply.github.com>
2023-12-17 15:15:42 +0000

    Finished Task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(whatString, repeatTimes) {
+    if (repeatTimes > -1){
+        return whatString.repeat(repeatTimes);
+    } else{
+        return 'ERROR'
+    }
+    
 
 };
 
