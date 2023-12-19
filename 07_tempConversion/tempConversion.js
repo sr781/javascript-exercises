8d9ac6fbb5a175b61cc20608a59065956412305d
sr781 <96390217+sr781@users.noreply.github.com>
2023-12-19 18:22:18 +0000

    Finished Task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  var celsius = (fahrenheit - 32) * (5/9);
+  return parseFloat(celsius.toFixed(1)); //Rounds to 1 decimal point
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  var fahrenheit = (celsius * (9/5)) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
