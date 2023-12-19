afc57fc0aac4f49e7673284e7edbce76d074d4f6
sr781 <96390217+sr781@users.noreply.github.com>
2023-12-19 17:08:18 +0000

    Finished Task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(leapYearCheck) {
+    if(leapYearCheck % 4 === 0){ //Checks to see if it is divisble by 4
+        if (leapYearCheck % 100 === 0){ //Checks to see if this is valid
+            if (leapYearCheck % 400 === 0){ //If the previous 'if' statement is valid, this one must be as well for it to be a leap year
+                return true;
+            } else{
+                return false;
+            }
+
+        }
+        return true;
+
+    }else{
+        return false;
+    }
 
 };
 
